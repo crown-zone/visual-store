de5faff23106e2225a7f25230520e278d28ab9c3
zhoumian <zhoumian@nsfocus.com>
2023-05-17 11:51:16 +0800

    Feat 项目配置

---
+++ src/config/index.ts
@@ -0,0 +1,17 @@
+import * as path from 'path';
+import * as fs from 'fs';
+import { parse } from 'yaml';
+
+type ApplicationEnv = 'production' | 'development';
+
+export const getApplicationEnv = (): ApplicationEnv => {
+    return process.env.APPLICATION_ENV as ApplicationEnv;
+};
+
+export const getApplicationConfig = () => {
+    const applicationEnv = getApplicationEnv();
+    const yamlPath = path.join(process.cwd(), `./resource/application.${applicationEnv}.yml`);
+    const yamlFile = fs.readFileSync(yamlPath, 'utf8');
+
+    return parse(yamlFile);
+};
